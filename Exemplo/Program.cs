6cfd86026d3bffb970d3fdeb584d2423f2d023a0
rafael.koji <rafaelkvb@gmail.com>
2023-05-28 20:48:53 -0300

    Passo 6 no trabalho, alteracao feita por Rafael Brunello

--- Exemplo/Program.cs
+++ Exemplo/Program.cs
@@ -26,7 +26,7 @@ class Program
     static void Main(string[] args)
     {
         Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
-        Warrior bob = new Warrior("Bob", 1000, 120, 40);
+        Warrior bob = new Warrior("Joao", 1000, 120, 40); // Outra alteração feita por Rafael Brunello
 
         Battle.StartFight(maximus, bob);
 
